31a374cbcfd64067c9e4180eddad638d68b0276d
monkeymon <monkeymon@gmail.com>
2020-05-16 21:08:28 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -72,42 +72,31 @@ class _LoginPageState extends State<LoginPage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      body: DecoratedBox (
+      body: Container(
+        decoration: BoxDecoration (
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage("../assets/loginBackground.jpg"), fit: BoxFit.cover),
+          image: DecorationImage(image: AssetImage("assets/loginBackground.jpg"), fit: BoxFit.cover),
         ),
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Row (
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            TextFormField(
+              decoration: InputDecoration(                  
+                labelText: 'CT Number'
+              )
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password'
+              )
+            )
           ],
-        ),
-      ),
+        )
+      ), 
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Increments',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
